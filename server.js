ade0994d68b95d36beae4ec737f3a2be877c1352
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-03-03 21:27:22 +0530

    basic

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    // ...
+});
+
+io.on("connection", (socket) => {
+    // ...
+});
+
+httpServer.listen(3000);
