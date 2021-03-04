26dd5ef1729a8aa7439725a27c8deee401e9df92
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-03-04 15:21:44 +0530

    Cors error resolved

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-import express from "express";
 import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-    // ...
+
+let io = new Server(httpServer,{
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
 });
 
-io.on("connection", (socket) => {
-    // ...
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('user connected');
 });
 
-httpServer.listen(3000);
+httpServer.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
