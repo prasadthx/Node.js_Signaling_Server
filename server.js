ba701e1c0601d7aba82751a552a24493580ef42c
prasad <prasadzore31@gmail.com>
2021-03-20 21:24:14 +0530

    working signaling server

--- server.js
+++ server.js
@@ -36,6 +36,48 @@ var time = 0;
 io.on('connection', (socket) => {
     time += 1
     console.log('user connected: '+time+'time');
+    socket.on("join",(roomName)=>{
+        console.log(roomName)
+        let rooms = io.sockets.adapter.rooms;
+        let room = rooms.get(roomName);
+        let roomStatus;
+        //room == undefined when no such room exists.
+        if (room === undefined) {
+            socket.join(roomName);
+            roomStatus = 'created'
+            socket.emit('joined',roomStatus);
+        } else if (room.size === 1) {
+            //room.size == 1 when one person is inside the room.
+            socket.join(roomName);
+            roomStatus = 'joined'
+            socket.emit('joined',roomStatus);
+        } else {
+            //when there are already two people inside the room.
+            socket.emit("full");
+        }
+        console.log(rooms);
+    });
+    socket.on("ready", (roomName) => {
+        socket.broadcast.to(roomName).emit("ready"); //Informs the other peer in the room.
+    });
+
+    //Triggered when server gets an icecandidate from a peer in the room.
+    socket.on("candidate", function (candidate, roomName) {
+        console.log(candidate);
+        socket.broadcast.to(roomName).emit("candidate", candidate); //Sends Candidate to the other peer in the room.
+    });
+
+    //Triggered when server gets an offer from a peer in the room.
+
+    socket.on("offer", function (offer, roomName) {
+        socket.broadcast.to(roomName).emit("offer", offer); //Sends Offer to the other peer in the room.
+    });
+
+    //Triggered when server gets an answer from a peer in the room.
+
+    socket.on("answer", function (answer, roomName) {
+        socket.broadcast.to(roomName).emit("answer", answer); //Sends Answer to the other peer in the room.
+    });
 });
 
 httpServer.listen(port, () => {
